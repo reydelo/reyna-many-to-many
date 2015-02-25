c33554e333a82d931720d710937b416f39037ca0
Reyna DeLoge <reyna.deloge@gmail.com>
2015-02-25 14:32:32 -0700

    user can create a company review

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -4,4 +4,27 @@ class CompaniesController < ApplicationController
     @companies = Company.all
   end
 
+  def show
+    @company = Company.find(params[:id])
+  end
+
+  def new
+    @company = Company.new
+  end
+
+
+    def create
+      @company = Company.new(company_params)
+      if @company.save
+        redirect_to companies_path
+      else
+        render :new, notice: 'Company form invalid'
+      end
+    end
+
+    private
+    def company_params
+      params.require(:company).permit(:name, :catch_phrase, :suffix)
+    end
+
 end
