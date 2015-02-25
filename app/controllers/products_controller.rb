1720aa662ead86cca3e2b25113ade3541b675ba7
Reyna DeLoge <reyna.deloge@gmail.com>
2015-02-25 14:19:43 -0700

    users can create new products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -10,8 +10,17 @@ class ProductsController < ApplicationController
 
 
     def create
+      @product = Product.new(product_params)
+      if @product.save
+        redirect_to root_path
+      else
+        render :new, notice: 'Product form invalid'
+      end
+    end
 
-      ### create a product here.
+    private
+    def product_params
+      params.require(:product).permit(:name, :price, :company_id)
     end
 
 end
